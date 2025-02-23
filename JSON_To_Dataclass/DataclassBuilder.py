6292a89fc89206826969a0485151e93e86dbca1b
Lifelite <j.t.huntsman@gmail.com>
2025-02-23 18:53:20 -0500

    Fixed bugs

--- JSON_To_Dataclass/DataclassBuilder.py
+++ JSON_To_Dataclass/DataclassBuilder.py
@@ -16,12 +16,13 @@ class DataclassBuilder:
         types = []
         for item in value:
             if type(item) == dict:
-                inner_dc = self.create_dataclass(value, key, inner=True)
-                if inner_dc not in self.inside_classes:
-                    self.inside_classes.append(inner_dc)
+                inner_dc = self.handle_dict(item)
+                data_string = f"class {key[0].upper() + key[1:]}:\n" + inner_dc
+                if data_string not in self.inside_classes:
+                    self.inside_classes.append(data_string)
                     types.append(key[0].upper() + key[1:])
             elif type(item) == list:
-                types.appent(self.handle_lists(key, item))
+                types.append(self.handle_lists(key, item))
             else:
                 evaluate_type = re.findall(r"'([^']*)'", str(type(item)))[0]
                 if evaluate_type not in types:
@@ -29,13 +30,13 @@ class DataclassBuilder:
 
         return f"{tab}{key}: list[{' | '.join(types)}]\n"
 
-    def handle_dict(self, data):
-        dataclass_output = ""
+    def handle_dict(self, data, name=None):
+        dataclass_output = f"class {name[0].upper() + name[1:]}:\n" if name else ''
         for key, value in data.items():
 
             if type(value) == dict:
                 dataclass_output += f"{tab}{key}: {key[0].upper() + key[1:]}\n"
-                self.inside_classes.append(self.create_dataclass(value, key, inner=True))
+                self.inside_classes.append(self.handle_dict(value, key))
             elif type(value) == list:
                 dataclass_output += self.handle_lists(key, value)
             else:
@@ -44,7 +45,7 @@ class DataclassBuilder:
 
         return dataclass_output
 
-    def create_dataclass(self, data=None, dataclass_name=None, inner=False):
+    def create_dataclass(self, data=None, dataclass_name=None):
         if data is None:
             data =self.data
         if dataclass_name is None:
@@ -56,13 +57,14 @@ class DataclassBuilder:
             dataclass_output += self.handle_dict(data)
 
         elif isinstance(data, list):
-            init_output = ''
             self.handle_lists(dataclass_name, data)
+            init_output = None
+            if self.inside_classes and len(self.inside_classes) > 0:
+                init_output = '\n'.join(self.inside_classes)
             return f"{init_output}"
 
+
         init_output = ''
-        if inner:
-            return f"{dataclass_output}"
         if self.inside_classes and len(self.inside_classes) > 0:
             init_output = '\n'.join(self.inside_classes)
         return f"{init_output}\n\n{dataclass_output}"
--- JSON_To_Dataclass/sample.py
+++ JSON_To_Dataclass/sample.py
@@ -1,27 +1,6 @@
-class GlossDef:
-	para: str
-	GlossSeeAlso: list[str]
-
-class GlossEntry:
-	ID: str
-	SortAs: str
-	GlossTerm: str
-	Acronym: str
-	Abbrev: str
-	GlossDef: GlossDef
-	GlossSee: str
-
-class GlossList:
-	GlossEntry: GlossEntry
-
-class GlossDiv:
-	title: str
-	GlossList: GlossList
-
-class Glossary:
-	title: str
-	GlossDiv: GlossDiv
-
-
 class GeneratedDataclass:
-	glossary: Glossary
+	name: str
+	language: str
+	id: str
+	bio: str
+	version: float
